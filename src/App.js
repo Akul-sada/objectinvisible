5bce6802c03ab2f5f9a39a94e65370f53eb77e2c
Akul-sada <sadananda.sherigara@gmail.com>
2024-12-22 15:56:12 +0530

    comment out second upload object

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import ImageUploadBox from './ImageUploadBoxInitial';
 function App() {
   // add photo upload functionality
   // 1) take permission of the camera device
+  const onSubmit =()=>{
+    window.location.reload() ;
+  }
   return (
     <div className="App">
       <div className='upload-boxes-parent'>
         <ImageUploadBox bgColor="red" borderColor="red" label="upload the object image"/>
         <ImageUploadBox bgColor="orange" borderColor="orange" label="upload the object's initial point"/>
-        <ImageUploadBox bgColor="yellow" borderColor="yellow" label="upload the object image"/>
+        {/* <ImageUploadBox bgColor="yellow" borderColor="yellow" label="upload the object image"/> */}
         <ImageUploadBox bgColor="blue" borderColor="blue" label="upload the object final point"/>
       </div>
       <div className="statement">
         <h2><span className='bg-red'>THIS OBJECT IS COMPLETELY OUT</span> <span className='bg-orange'>FROM THIS  POINT  <span>&amp;</span></span> <span className='bg-blue'>COMPLETELY IN AT THIS POINT  <span style={{fontSize:'9rem'}}>&infin;</span></span></h2>
+      </div>
+      <div>
+          <input onClick={onSubmit} type="submit" className='button-71' value="Submit"/>
       </div>
     </div>
   );
--- src/ImageUploadBoxInitial.js
+++ src/ImageUploadBoxInitial.js
@@ -1,33 +1,35 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import './InputSubmitButton.css';
 
 
-const ImageUploadBox =({bgColor,borderColor,label})=>{
-    const [image, setImage]= useState(null);
-    const [error, setError]= useState(null);
+
+const ImageUploadBox = ({ bgColor, borderColor, label }) => {
+    const [image, setImage] = useState(null);
+    const [error, setError] = useState(null);
     const fileInputRef = useRef(null);
 
-    const handleImageUpload = (event)=>{
+    const handleImageUpload = (event) => {
         const file = event.target.files[0];
         console.log(file);
 
         // Image Validation
-        if(file){
-            const validTypes = ["image/jpeg","image/png","image/gif"];
-            if(!validTypes.includes(file.type)){
+        if (file) {
+            const validTypes = ["image/jpeg", "image/png", "image/gif"];
+            if (!validTypes.includes(file.type)) {
                 setError("Invalid file type. Please upload an Image.");
                 return;
             }
             // Check file size (e.g., max 12 MB)
-            const maxSize = 12*1024*1024;
-            if(file.size > maxSize){
+            const maxSize = 12 * 1024 * 1024;
+            if (file.size > maxSize) {
                 setError("File is too large. Maximum size allowed is 12MB.");
                 return;
             }
 
             // Read and set Image
             const reader = new FileReader();
-            reader.onloadend = ()=>{
+            reader.onloadend = () => {
                 setImage(reader.result);
                 setError(null);
             }
@@ -35,17 +37,20 @@ const ImageUploadBox =({bgColor,borderColor,label})=>{
 
         }
     }
-    const handleClick = ()=>{
+    const handleClick = () => {
         fileInputRef.current.click();
     }
-    
-    return(
+
+    return (
         <>
-            <div className={`upload-box bg-${bgColor} border-${borderColor}`} onClick={handleClick}>
-                <input required type='file' ref={fileInputRef} onChange={handleImageUpload} accept="image/jpeg,image/png,image/gif" style={{display:'none'}}/>
-                {error && <div className='error-message'>{error}</div>}
-                {!image ? (<div className='upload-text'>{label}</div>):(<img style={{objectFit:'cover',width:'100%',height:'100%'}} src={image} alt="uploaded-image"/>)}
-            </div>
+            <form>
+                <div className={`upload-box bg-${bgColor} border-${borderColor}`} onClick={handleClick}>
+                    <input required type='file' ref={fileInputRef} onChange={handleImageUpload} accept="image/jpeg,image/png,image/gif" style={{ display: 'none' }} />
+                    {error && <div className='error-message'>{error}</div>}
+                    {!image ? (<div className='upload-text'>{label}</div>) : (<img style={{ objectFit: 'cover', width: '100%', height: '100%' }} src={image} alt="uploaded-image" />)}
+                </div>
+                
+            </form>
         </>
     );
 
