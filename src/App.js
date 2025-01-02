236b6f07b1ee4dd8c5941bfbc1f389419d76e8e8
Akul-sada <sadananda.sherigara@gmail.com>
2025-01-02 10:29:05 +0530

    add morse code to the project

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
     <div className="App">
       <div className='upload-boxes-parent'>
         <ImageUploadBox bgColor="red" borderColor="red" label="upload the object image"/>
-        <ImageUploadBox bgColor="orange" borderColor="orange" label="upload the object's initial point"/>
+        {/* <ImageUploadBox bgColor="orange" borderColor="orange" label="upload the object's initial point"/> */}
         {/* <ImageUploadBox bgColor="yellow" borderColor="yellow" label="upload the object image"/> */}
         <ImageUploadBox bgColor="blue" borderColor="blue" label="upload the object final point"/>
       </div>
       <div className="statement">
-        <h2><span className='bg-red'>THIS OBJECT IS COMPLETELY OUT</span> <span className='bg-orange'>FROM THIS  POINT  <span>&amp;</span></span> <span className='bg-blue'>COMPLETELY IN AT THIS POINT  <span style={{fontSize:'9rem'}}>&infin;</span></span></h2>
+        <h2><span className='bg-red'>-·....·..·...·---·-...·.---·.·-.-.·-·..·...·-.-.·---·--·.--.·.-..·.·-·.·.-..·-.--·---·..-·-</span> <span className='bg-red'>..-.·.-.·---·--·-·....·..·...·.--.·---·..·-.·-  <span>&amp;</span></span> <span className='bg-blue'>-.-.·---·--·.--.·.-..·.·-·.·.-..·-.--·..·-.·.-·-·-·....·..·...·.--.·---·..·-.·-  <span style={{fontSize:'9rem'}}>&infin;</span></span></h2>
       </div>
       <div>
           <input onClick={onSubmit} type="submit" className='button-71' value="Submit"/>
